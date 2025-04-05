0ea6f398cbc59c91971996c8140f781e80cfcf21
Riad El Fassi <riel-fas@c2r4p6.1337.ma>
2025-04-05 05:57:16 +0000

    eerr

--- philo/parsing.c
+++ philo/parsing.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: riel-fas <riel-fas@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: riel-fas <riel-fas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/05 03:30:56 by riel-fas          #+#    #+#             */
-/*   Updated: 2025/04/05 04:41:26 by riel-fas         ###   ########.fr       */
+/*   Updated: 2025/04/05 05:22:12 by riel-fas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,54 +17,43 @@
 
 static	char	*check_input(char *input)
 {
-    int		x;
-    char	*number;
+	int		x;
+	char	*number;
 
-    x = 0;
-    while ((input[x] >= 9 && input[x] <= 13) || input[x] == 32)
-        x++;
-    if (input[x] == '+')
-        x++;
-    else if (input[x] == '-')
-        error_mes_exit("ERROR\nINPUT CANT BE NEGATIVE\n");
-    if (!(input[x] >= '0' && input[x] <= '9'))
-        error_mes_exit("ERROR\nINPUT MUST BE NUMBERS\n");
-    number = input + x;
-    x = 0;
-    while (number[x] >= '0' && number[x] <= '9')
-        x++;
-    if (x > 10)
-        error_mes_exit("ERROR\nTHE INPUT IS BIGGER THAN INT_MAX");
-    return (number);
+	x = 0;
+	while ((input[x] >= 9 && input[x] <= 13) || input[x] == 32)
+		x++;
+	if (input[x] == '+')
+		x++;
+	else if (input[x] == '-')
+		error_mes_exit("		🚦ERROR🚦\n⛔️INPUT CANT BE NEGATIVE\n⛔️");
+	if (!(input[x] >= '0' && input[x] <= '9'))
+		error_mes_exit("		🚦ERROR🚦\n		⛔️INPUT MUST BE NUMBERS\n⛔️");
+	number = input + x;
+	x = 0;
+	while (number[x] >= '0' && number[x] <= '9')
+		x++;
+	if (x > 10)
+		error_mes_exit("		🚦ERROR🚦\n		⛔️THE INPUT IS BIGGER THAN INT_MAX⛔️");
+	return (number);
 }
-// static long	handle_overflow(int sign)
-// {
-// 	if (sign == 1)
-// 	return (LONG_MAX);
-// }
 
 static long	atoi_v2(char *s)
 {
-    int		x;
-    long	result;
-    // int		sign;
-    // long	prev_result;
+	int		x;
+	long	result;
 
-    s = check_input(s);
-    result = 0;
-    x = 0;  // Add initialization
-    // sign = 1;
-    while (s[x] >= '0' && s[x] <= '9')
-    {
-        // prev_result = result;
-        result = result * 10 + (s[x] - '0');
-        // if (result / 10 != prev_result)
-        // 	return (handle_overflow(sign));
-        x++;
-    }
-    if (result > INT_MAX)
-        error_mes_exit("ERROR\nINT_MAX IS THE LIMIT");
-    return (result);
+	s = check_input(s);
+	result = 0;
+	x = 0;  // Add initialization
+	while (s[x] >= '0' && s[x] <= '9')
+	{
+		result = result * 10 + (s[x] - '0');
+		x++;
+	}
+	if (result > INT_MAX)
+		error_mes_exit("		🚦ERROR🚦\n		⛔️INT_MAX IS THE LIMIT⛔️");
+	return (result);
 }
 
 ///////////////./philo xxx //// xxx //// xxx  //// xxx //// [x]/////////////a philosopher is a struct and forks are mutex
@@ -72,6 +61,17 @@ static long	atoi_v2(char *s)
 void	input_pars(t_args *input, char **av)
 {
 	input->philo_nbr = atoi_v2(av[1]);
+	//in miliseconds
+	input->time_to_die = atoi_v2(av[2]) * 1000;
+	input->time_to_eat = atoi_v2(av[3]) * 1000;
+	input->time_to_sleep = atoi_v2(av[4]) * 1000;
+
+	if (input->time_to_die < 60000 || input->time_to_eat < 60000 || input->time_to_sleep < 60000)
+		error_mes_exit("		🚦ERROR🚦\n		⛔️TIME SHOULD BE MORE THAN 60 MILLI.SE.⛔️");
+	if (av[5])
+		input->meals_limit = atoi_v2(av[5]);
+	else
+		input->meals_limit = 0;
 
 
 
--- philo/philo_main.c
+++ philo/philo_main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo_main.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: riel-fas <riel-fas@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: riel-fas <riel-fas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/02 15:12:10 by riel-fas          #+#    #+#             */
-/*   Updated: 2025/04/05 04:12:21 by riel-fas         ###   ########.fr       */
+/*   Updated: 2025/04/05 05:21:42 by riel-fas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ int	main(int ac, char **av)
 	// 	free_leak(&input);
 	}
 	else
-		error_mes_exit("		🚦ERROR🚦\n⛔️INPUT SHOULD BE : ./philo xx xxx xxx xxx [xxx]");
+		error_mes_exit("		🚦ERROR🚦\n⛔️INPUT SHOULD BE : ./philo xx xxx xxx xxx [xxx]⛔️");
 
 
 
